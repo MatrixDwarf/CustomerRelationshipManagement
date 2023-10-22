a73c83b934f7ceda3a6a1d1ccfd115c1df9bc5d3
Caio Galvao <caio.amorozo@hotmail.com>
2023-10-22 13:59:45 -0300

    Creating Front-End part (visuals only, subject to change) and basic forms

---
+++ webapp/forms.py
@@ -0,0 +1,19 @@
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.models import UserManager
+
+from django import forms
+from django.forms.widgets import PasswordInput, TextInput
+
+# - Registering / Creating an User
+
+class CreateUserForm(UserCreationForm):
+
+    class Meta:
+        model = User
+        fields = ['username' 'password1', 'password2']
+
+# - Logging in
+
+class LoginForm(AuthenticationForm):
+    username = forms.CharField(widget=TextInput())
+    password = forms.CharField(widget=PasswordInput())
